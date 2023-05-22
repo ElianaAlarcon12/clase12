b1fd0db16e4777c5b395ea66be37ee04206a7cf7
eliana <elarcon.digital@gmail.com>
2023-05-22 12:23:48 -0300

    Tarea

---
+++ Js/index.js
@@ -0,0 +1,20 @@
+
+
+// let entrada = prompt('Ingresar un nombre en mayúsculas');
+// // repetir hasta que se ingrese "ESC"
+// while (entrada !== 'ESC') {
+//   switch (entrada) {
+//     case 'ANA':
+//       alert('Hola ANA');
+//       break;
+//     case 'MIGUEL':
+//       alert('Hola MIGUEL');
+//       break;
+//     default:
+//       alert('¿Quién eres?');
+//       break;
+//   }
+//   entrada = prompt('Ingresar un nombre en mayúsculas');
+// }
+
+let entrada 
